9034be21ba5209079dc6765cd7273f4e4903e8fa
James Virtudazo <virtjameslourd55@gmail.com>
2026-02-12 17:45:32 +0800

    fix(io): IO_to_start_at_0
    I thought that IOx would have to start at 1 because it would be used to
    index pins 1-16 but the pins are actually index 0-15, even in pin names
    like PCx (Port C 13).

--- src/drivers/io.c
+++ src/drivers/io.c
@@ -2,28 +2,25 @@
 #include "stm32f401xc.h"
 
 void io_configure_pin(GPIO_TypeDef *port, const uint32_t pin, const io_pin_config_t *config) {
-  port->MODER &= ~(3U << (pin * 2));
-  port->MODER |= (config->mode << (pin * 2));
-
-  port->PUPDR &= ~(3U << (pin * 2));
-  port->PUPDR |= (config->pupd << (pin * 2));
+  port->MODER &= ~(3U << (pin * 2U));
+  port->MODER |= (config->mode << (pin * 2U));
 
   port->OTYPER &= ~(1U << (pin));
   port->OTYPER |= (config->otype << pin);
 
-  port->OSPEEDR &= ~(3U << (pin * 2));
-  port->OSPEEDR |= (config->speed << (pin * 2));
+  port->OSPEEDR &= ~(3U << (pin * 2U));
+  port->OSPEEDR |= (config->speed << (pin * 2U));
 
-  port->PUPDR &= ~(3U << (pin * 2));
-  port->PUPDR |= (config->pupd << (pin * 2));
+  port->PUPDR &= ~(3U << (pin * 2U));
+  port->PUPDR |= (config->pupd << (pin * 2U));
 
-  uint32_t idx = pin >> 3; // Divide by 8
-  uint32_t shift = (pin & 0x07) << 2; // (pin % 8) * 4 (since each pin uses 4 bits)
+  uint32_t idx = pin >> 3U; // Divide by 8
+  uint32_t shift = (pin & 0x07) << 2U; // (pin % 8) * 4 (since each pin uses 4 bits)
 
   // Only set the AFR if mode is actually set to alternate mode
-  if (config->mode == 0x2) {
+  if (config->mode == IO_MODE_ALTERNATE) {
     port->AFR[idx] &= ~(0xFU << shift);
-    port->AFR[idx] |= (config->altf);
+    port->AFR[idx] |= (config->altf << shift);
   }
 }
 
@@ -34,5 +31,9 @@ void io_toggle_pin(GPIO_TypeDef *port, const uint32_t pin) {
 void io_set_select();
 void io_set_direction();
 void io_set_resistor();
-void io_set_out(); 
+void io_set_out(GPIO_TypeDef *port, const uint32_t pin_mask, const io_out_e state) {
+  uint32_t shift = (state == IO_OUT_LOW) ? 0 : 16;
+  // Shift registers to second set of 16 bits if reset
+  port->BSRR = (pin_mask << shift); 
+} 
 io_in_e io_get_input();
